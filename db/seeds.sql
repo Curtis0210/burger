2304db4cd9c818e81cd0a54869c8f01278ded4ed
Curtis0210 <curtis0210@gmail.com>
2017-10-26 23:04:02 -0700

    new

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Arizona Burger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Burger');
